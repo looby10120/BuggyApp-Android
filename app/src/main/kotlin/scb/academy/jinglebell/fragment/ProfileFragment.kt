789414ad34afe687f1e22c60bc0980bd82c8af37
looby10120 <mosfb@hotmail.com>
2019-07-11 16:10:47 +0700

    Add setOnKeyListener in ProfileFragment

--- app/src/main/kotlin/scb/academy/jinglebell/fragment/ProfileFragment.kt
+++ app/src/main/kotlin/scb/academy/jinglebell/fragment/ProfileFragment.kt
@@ -5,11 +5,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_profile.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,15 +21,45 @@ import scb.academy.jinglebell.R
 import scb.academy.jinglebell.adapter.CountryAdapter
 import scb.academy.jinglebell.service.ApiManager
 import scb.academy.jinglebell.vo.Country
+import android.view.KeyEvent.KEYCODE_ENTER
+import android.view.KeyEvent.KEYCODE_DPAD_CENTER
+import android.view.KeyEvent
+
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var nickname: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nickname = view.findViewById(R.id.mNicknameEditText)
+        nickname.apply {
+
+            nickname.setOnKeyListener(object : View.OnKeyListener {
+                override fun onKey(v: View, keyCode: Int, event: KeyEvent): Boolean {
+                    if (event.action == KEYCODE_ENTER) {
+//                        Log.d("name", "hello")
+                        Toast.makeText(context, "hello", Toast.LENGTH_SHORT).show()
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+            })
+
+        }
+    }
 
 }
+
+
+
